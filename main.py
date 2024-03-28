9594f120a179f1746e2bb5f0703ac088e33a0507
Nathan Souza <naathanmarinho11@gmail.com>
2024-03-27 23:04:09 -0300

    add comentarios

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ produtos = []
 # Rota para a página inicial
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
+    if request.method == 'POST':  # Verifica se a requisição é do tipo POST
+        # Obtém os dados do formulário enviado
         nome = request.form['nome']
         descricao = request.form['descricao']
         valor = request.form['valor']
         disponivel = request.form['disponivel']
 
+        # Cria um dicionário para representar o produto
         produto = {
             'nome': nome,
             'descricao': descricao,
@@ -21,13 +23,17 @@ def index():
             'disponivel': disponivel
         }
 
+        # Adiciona o produto à lista de produtos
         produtos.append(produto)
+
+        # Redireciona de volta para a página inicial após a submissão do formulário
         return redirect(url_for('index'))
 
-    # Ordena os produtos pelo valor
+    # Ordena os produtos pelo valor antes de exibi-los
     produtos_ordenados = sorted(produtos, key=lambda x: float(x['valor']), reverse=True)
 
+    # Renderiza o template 'index.html' passando a lista de produtos ordenados
     return render_template('index.html', produtos=produtos_ordenados)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # Executa a aplicação Flask em modo de depuração
