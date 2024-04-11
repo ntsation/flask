c1a9fff073c0d89f3cd553c6e8247ccacccd0ee1
Nathan Souza <naathanmarinho11@gmail.com>
2024-04-10 22:25:11 -0300

    adiciona melhorias no formulario

--- main.py
+++ main.py
@@ -10,24 +10,33 @@ produtos = []
 def index():
     if request.method == 'POST':  # Verifica se a requisição é do tipo POST
         # Obtém os dados do formulário enviado
-        nome = request.form['nome']
-        descricao = request.form['descricao']
-        valor = request.form['valor']
-        disponivel = request.form['disponivel']
+        nome = request.form.get('nome')
+        descricao = request.form.get('descricao')
+        valor = request.form.get('valor')
+        disponivel = request.form.get('disponivel')
 
-        # Cria um dicionário para representar o produto
-        produto = {
-            'nome': nome,
-            'descricao': descricao,
-            'valor': valor,
-            'disponivel': disponivel
-        }
+        # Verifica se todos os campos foram preenchidos
+        if nome and descricao and valor and disponivel:
+            try:
+                valor = float(valor)  # Converte o valor para float
+                # Cria um dicionário para representar o produto
+                produto = {
+                    'nome': nome,
+                    'descricao': descricao,
+                    'valor': valor,
+                    'disponivel': disponivel
+                }
 
-        # Adiciona o produto à lista de produtos
-        produtos.append(produto)
+                # Adiciona o produto à lista de produtos
+                produtos.append(produto)
 
-        # Redireciona de volta para a página inicial após a submissão do formulário
-        return redirect(url_for('index'))
+                # Redireciona de volta para a página inicial após a submissão do formulário
+                return redirect(url_for('index'))
+            except ValueError:
+                return "Erro: O valor do produto deve ser numérico."
+
+        else:
+            return "Erro: Todos os campos devem ser preenchidos."
 
     # Ordena os produtos pelo valor antes de exibi-los
     produtos_ordenados = sorted(produtos, key=lambda x: float(x['valor']), reverse=True)
